8fa6b175e3584f23a8a028556abbe92f586082b8
GiovanniRanzato <giovanni.ranzato2010@gmail.com>
2020-10-04 16:08:26 +0200

    added autoloader for classes

---
+++ admin/includes/functions.php
@@ -0,0 +1,15 @@
+<?php
+/* Autoload function will automatically load files from $the_path and including if non included yet */
+function classAutoLoader ($class)
+{
+    $class = strtolower($class);
+    $the_path = "includes/{$class}.php";
+    if (is_file($the_path)  && !class_exists($class) ) {
+        include $the_path;
+    } else {
+        die("Impossible to find file: {$class}.php");
+    }
+}
+spl_autoload_register('classAutoLoader');
+
+?>
